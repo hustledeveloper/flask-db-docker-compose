e896cdeca63afc0dabe945b8bce119a00a8ddb69
Ömer Faruk Eren <faruk008887@gmail.com>
2024-03-08 17:55:08 +0300

    a

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Pgadmin Postgre Docker Compose
-1. pgadmin server adresi bende sizde de kaçsa onunla servera bağlanın, konteynerin değil bridgenin ip adresini girin 
-şunun gibi muhtemelen, terminaldan bakın kalkınca 172.19.0.1, 172.20.0.1 gibi
+1. pgadmin server adresi sizde de kaçsa onunla servera bağlanın, konteynerin değil bridge'in ip adresini girin 
+şunun gibi muhtemelen:
+172.19.0.1 veya 172.20.0.1 
+terminaldan kontrol edebilirsiniz
 2. kullanıcı adı şifre db hepsine postgres yazın
-3. ilk girişte username ve passwork
+
+3. ilk girişte username ve password http://localhost:8080/
+
 your_email@example.com
 your_password
 ----
-cahilseniz, 
+### _Docker'ı çalıştırmak_
+ 
 dokcer-compose up
 docker-compose stop
 docker ps -a (kontrol)
 -----
-http://localhost:8080/
-(pgadmin)
+
 
--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, request
-import json
-from redis import Redis
-from flask import Flask, jsonify, request 
+import re
+from flask import Flask, jsonify, render_template, request
 
 
 app = Flask(__name__)
@@ -11,6 +9,31 @@ def helloworld():
     if(request.method == 'GET'): 
         data = {"data": "Hello World"} 
         return jsonify(data)
+    
+@app.route('/operation', methods=["POST"])
+def calculate():
+    num1 = request.form['number1']
+    op = request.form['operator']
+    num2 = request.form['number2']
+    
+    result = 1
+    if op == '+':
+          result = int(num1) + int(num2);
+   
+    if op == '-':
+          result = int(num1) - int(num2);
+
+    if op == '*':
+          result = int(num1) * int(num2);
+
+    if op == '/':
+          result = int(num1) / int(num2);
+
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
